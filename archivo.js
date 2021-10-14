20dd210ee1e34b0d799d2f4368c9855b73ec973f
Sol <slereah@gmail.com>
2021-10-14 11:07:46 -0300

    modifiqué el evento y la forma de mostrar el resultado

--- archivo.js
+++ archivo.js
@@ -1,33 +1,48 @@
 'use strict'
 
-
-
-
+let btn = document.getElementById('btn-conversor') 
 
 
 function conversorDolares(){
-    let dolarHoy= 104.19  
-    let ingresoDolares = document.getElementById("dolares").value
-    let resultado = ingresoDolares * dolarHoy 
-    
+    let ingresoDolares = document.getElementById('dolares').value
     let conversion = document.getElementById('pesos')
     let impuestoPais = document.getElementById('impuesto-pais')
     let rentencionGanacias = document.getElementById('ganancias')
-
     let resultadoTotal = document.getElementById('total')
+    
+    let dolarHoy= 105.18  
+    let resultado = ingresoDolares * dolarHoy 
     let sumaResultados = (resultado + (resultado * 30)/100 + (resultado *35)/100).toFixed(2)
 
     if(resultado<=0){
         alert('No se puede calcular eso')
         }
         else{
-            conversion.innerHTML = resultado
-            impuestoPais.innerHTML = (resultado * 30)/100
-            rentencionGanacias.innerHTML = (resultado *35)/100
-            resultadoTotal.innerHTML = sumaResultados
+            conversion.innerHTML =`$${resultado}` 
+            impuestoPais.innerHTML = `$${(resultado * 30)/100}`
+            rentencionGanacias.innerHTML = `$${(resultado *35)/100}`
+            resultadoTotal.innerHTML = `$${sumaResultados}`
         }
 
 }
+
+btn.addEventListener('click', conversorDolares)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
+
+
   
     
